191beb75d4d479ce47c47c698817edf8c014070e
Khushal Rathore <118611135+khushalrathore@users.noreply.github.com>
2024-03-03 10:52:17 +0530

    march3 homeUpdate

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
   var header = document.querySelector('header');
   var section = document.querySelector('section');
-  var sectionOffset = section.offsetTop;
-  var distanceFromSection = 1 * parseFloat(getComputedStyle(document.documentElement).fontSize);
+  
+  if (section) {
+    var sectionOffset = section.offsetTop;
+    var distanceFromSection = 0.1 * parseFloat(getComputedStyle(document.documentElement).fontSize);
 
-  window.addEventListener('scroll', function() {
-    if (window.scrollY > sectionOffset + distanceFromSection) {
-      header.style.backgroundColor = 'black';
-    } else {
-      header.style.backgroundColor = 'transparent';
-    }
-  });
+    window.addEventListener('scroll', function() {
+      if (window.scrollY > sectionOffset + distanceFromSection) {
+        header.style.backgroundColor = 'black';
+      } else {
+        header.style.backgroundColor = 'transparent';
+      }
+    });
+  } 
 });
