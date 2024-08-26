1d6881bbf24463ba77466dd2f8a7903fff1ede2a
Khushal Rathore <khushalrathore@users.noreply.github.com>
2024-08-26 17:17:06 +0530

    Files

--- src/Components/Courses/coursesData.jsx
+++ src/Components/Courses/coursesData.jsx
@@ -6,7 +6,7 @@ export const coursesInfo = [
         icon: icon1,
         time: '<1hr',
         desc: `Ye course pakka se video editor bana dega jada socho mat lelo or kuch sikh lo, kuki syd ye paise vase bhi cigrattee me udaa doge isse acha kuch use lelo. Padhayi to kr li jitni krni thi apni skill pe focus kr ke aage baddo or professional ban jaooooooooo`,
-        price: 1,
+        price: 199,
         link: "https://mega.nz/file/cisxRA7Z#kRVzdM2_h_MIk92uWpKz7F7wDdZUCf29fe4IKmhH3c8"
     }
 ];
